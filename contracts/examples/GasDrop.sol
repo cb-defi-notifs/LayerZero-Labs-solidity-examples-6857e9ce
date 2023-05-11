4e19daf4080703e8bc4bfa5777d996b9b9b48654
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-05-11 15:40:20 -0700

    removing single send

--- contracts/examples/GasDrop.sol
+++ contracts/examples/GasDrop.sol
@@ -13,10 +13,9 @@ contract GasDrop is NonblockingLzApp {
 
     constructor(address _endpoint) NonblockingLzApp(_endpoint) {}
 
-    function estimateSendFee(uint16 _dstChainId, bytes memory _toAddress, uint _amount, bool _useZro) external view virtual returns (uint nativeFee, uint zroFee) {
-        bytes memory adapterParams = abi.encodePacked(VERSION, dstGas, _amount, _toAddress);
-        bytes memory payload = abi.encode(_amount, msg.sender, _toAddress);
-        return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, adapterParams);
+    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory, uint64, bytes memory _payload) internal virtual override {
+        (uint amount, address fromAddress, bytes memory toAddress) = abi.decode(_payload, (uint, address, bytes));
+        emit ReceiveGasDrop(_srcChainId, fromAddress, toAddress, amount);
     }
 
     function estimateSendFee(uint16[] memory _dstChainId, bytes[] memory _toAddress, uint[] memory _amount, bool _useZro) external view virtual returns (uint nativeFee, uint zroFee) {
@@ -29,18 +28,6 @@ contract GasDrop is NonblockingLzApp {
             nativeFee += native;
             zroFee += zro;
         }
-    }
-
-    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory, uint64, bytes memory _payload) internal virtual override {
-        (uint amount, address fromAddress, bytes memory toAddress) = abi.decode(_payload, (uint, address, bytes));
-        emit ReceiveGasDrop(_srcChainId, fromAddress, toAddress, amount);
-    }
-
-    function gasDrop(uint16 _dstChainId, bytes memory _toAddress, uint _amount, address payable _refundAddress, address _zroPaymentAddress) external payable virtual {
-        bytes memory adapterParams = abi.encodePacked(VERSION, dstGas, _amount, _toAddress);
-        bytes memory payload = abi.encode(_amount, msg.sender, _toAddress);
-        _lzSend(_dstChainId, payload, _refundAddress, _zroPaymentAddress, adapterParams, msg.value);
-        emit SendGasDrop(_dstChainId, msg.sender, _toAddress, _amount);
     }
 
     function gasDrop(uint16[] memory _dstChainId, bytes[] memory _toAddress, uint[] memory _amount, address payable _refundAddress, address _zroPaymentAddress) external payable virtual {
