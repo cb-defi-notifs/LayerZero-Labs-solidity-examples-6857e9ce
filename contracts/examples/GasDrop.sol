17881a32e4d7109b31a09801eb6d712f5ed4861a
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2023-05-11 15:36:05 -0700

    adding multisend

--- contracts/examples/GasDrop.sol
+++ contracts/examples/GasDrop.sol
@@ -19,7 +19,19 @@ contract GasDrop is NonblockingLzApp {
         return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, adapterParams);
     }
 
-    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory _srcAddress, uint64 _nonce, bytes memory _payload) internal virtual override {
+    function estimateSendFee(uint16[] memory _dstChainId, bytes[] memory _toAddress, uint[] memory _amount, bool _useZro) external view virtual returns (uint nativeFee, uint zroFee) {
+        require(_dstChainId.length == _toAddress.length, "_dstChainId and _toAddress must be same size");
+        require(_toAddress.length == _amount.length, "_toAddress and _amount must be same size");
+        for(uint i = 0; i < _dstChainId.length; i++) {
+            bytes memory adapterParams = abi.encodePacked(VERSION, dstGas, _amount[i], _toAddress[i]);
+            bytes memory payload = abi.encode(_amount[i], msg.sender, _toAddress[i]);
+            (uint native, uint zro) = lzEndpoint.estimateFees(_dstChainId[i], address(this), payload, _useZro, adapterParams);
+            nativeFee += native;
+            zroFee += zro;
+        }
+    }
+
+    function _nonblockingLzReceive(uint16 _srcChainId, bytes memory, uint64, bytes memory _payload) internal virtual override {
         (uint amount, address fromAddress, bytes memory toAddress) = abi.decode(_payload, (uint, address, bytes));
         emit ReceiveGasDrop(_srcChainId, fromAddress, toAddress, amount);
     }
@@ -31,7 +43,21 @@ contract GasDrop is NonblockingLzApp {
         emit SendGasDrop(_dstChainId, msg.sender, _toAddress, _amount);
     }
 
+    function gasDrop(uint16[] memory _dstChainId, bytes[] memory _toAddress, uint[] memory _amount, address payable _refundAddress, address _zroPaymentAddress) external payable virtual {
+        require(_dstChainId.length == _toAddress.length, "_dstChainId and _toAddress must be same size");
+        require(_toAddress.length == _amount.length, "_toAddress and _amount must be same size");
+        for(uint i = 0; i < _dstChainId.length; i++) {
+            bytes memory adapterParams = abi.encodePacked(VERSION, dstGas, _amount[i], _toAddress[i]);
+            bytes memory payload = abi.encode(_amount[i], msg.sender, _toAddress[i]);
+            address payable refundAddress = (i == _dstChainId.length - 1) ? _refundAddress : payable(address(this));
+            _lzSend(_dstChainId[i], payload, refundAddress, _zroPaymentAddress, adapterParams, address(this).balance);
+            emit SendGasDrop(_dstChainId[i], msg.sender, _toAddress[i], _amount[i]);
+        }
+    }
+
     function setDstGas(uint _dstGas) external onlyOwner {
         dstGas = _dstGas;
     }
+
+    receive() external payable {}
 }
